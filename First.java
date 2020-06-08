0f37de42d85c09d9ae571615abca2547143f6a17
Krishna1817 <66627528+Krishna1817@users.noreply.github.com>
2020-06-08 19:39:51 +0530

    some worst of me

---
+++ First.java
@@ -0,0 +1,14 @@
+package myproject;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		for (int i=1;i < 5; i++) {
+			System.out.println("Hello world"+i);
+			
+		}
+	}
+
+}
